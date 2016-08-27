ec8ed970a8eb9202792e306e2fde247124b404a4
Paul Lewis <paullewi@cisco.com>
2016-08-27 16:13:17 -0400

    Added Spark API python scripts

--- sparkMember.py
+++ sparkMember.py
@@ -5,8 +5,8 @@ import requests
 import json
 
 #variables
-url = 'https://api.ciscospark.com/v1/rooms'
-payload = {'title': 'TEST ROOM'}
+url = 'https://api.ciscospark.com/v1/memberships'
+payload = {	"roomId": "Y2lzY29zcGFyazovL3VzL1JPT00vMTNmY2VmNjAtNmM5MS0xMWU2LTkyZWUtZjFkNjY3OTc1NjFl", "personEmail": "badams2@cisco.com", "isModerator": "false"}
 headers = {'content-type': 'application/json', 'authorization':'Bearer NTJiMTQyZjUtZTZlZC00N2I1LThjNTgtYWNmMjgyMDJhZmMyYzExYzY3YjQtNTdl'}
 
 #API POST to create Spark Room
--- sparkMessage.py
+++ sparkMessage.py
@@ -5,8 +5,8 @@ import requests
 import json
 
 #variables
-url = 'https://api.ciscospark.com/v1/rooms'
-payload = {'title': 'TEST ROOM'}
+url = 'https://api.ciscospark.com/v1/messages'
+payload = {"roomId" : "Y2lzY29zcGFyazovL3VzL1JPT00vMTNmY2VmNjAtNmM5MS0xMWU2LTkyZWUtZjFkNjY3OTc1NjFl","text" : "Staff Member please report to isle X, pronto!"}
 headers = {'content-type': 'application/json', 'authorization':'Bearer NTJiMTQyZjUtZTZlZC00N2I1LThjNTgtYWNmMjgyMDJhZmMyYzExYzY3YjQtNTdl'}
 
 #API POST to create Spark Room
